4006ed93ef6c4293c059b2b0b9fdd86f0cb2105b
ginzabe <admin@ginzabe.com>
2024-04-20 22:47:29 -0400

    add whats up file

--- ginzabaJavaBasic/src/ginzabeScript/Array1Class.java
+++ ginzabaJavaBasic/src/ginzabeScript/Array1Class.java
@@ -2,30 +2,22 @@ package ginzabeScript;
 
 public class Array1Class {
 
-	
 	public static void main(String[] args) {
-		
-		int []  studentMarks = {75,86,56,43,90,97,45,34,56,99,78,45};
-		
+
+		int[] studentMarks = { 75, 86, 56, 43, 90, 97, 45, 34, 56, 99, 78, 45 };
+
 		int maxvalue = studentMarks[0];
-		for(int g=1;g<studentMarks.length;g++) {
-			   
-			if( maxvalue >   studentMarks[g] ) {
-				
-				
+		for (int g = 1; g < studentMarks.length; g++) {
+
+			if (maxvalue > studentMarks[g]) {
+
 				maxvalue = studentMarks[g];
-				
+
 			}
-			
-			
-			
+
 		}
-		
-		System.out.println("Maximum marks of student:"+maxvalue);
-		
-		
-		
-		
-		
+
+		System.out.println("Maximum marks of student:" + maxvalue);
+
 	}
 }
