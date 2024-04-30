87cd4dca99d5356bc9987a6a40c51c1585bbbafb
ginzabe <admin@ginzabe.com>
2024-04-29 20:38:15 -0400

    update rest assured package scripts and create pass/fail scenario

--- SeleniumTestngRestAssuredJdbc/src/test/java/RestAssuredwithJsonSimple/postRequest.java
+++ SeleniumTestngRestAssuredJdbc/src/test/java/RestAssuredwithJsonSimple/postRequest.java
@@ -1,5 +1,8 @@
 package RestAssuredwithJsonSimple;
 
+import static org.testng.Assert.assertEquals;
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
@@ -15,9 +18,22 @@ public class postRequest {
 		// get API request end point
 		Response response = RestAssured.get("https://reqres.in/api/users?page=2");
 
-		// print get status code and time
+		// print get status code
+		//get execution time
+		//get body
+		//get status line like 200 ok
+		//get header with content type for response format
+		
 		System.out.println(response.getStatusCode());
 		System.out.println(response.getTime());
+		System.out.println(response.getBody().asPrettyString());
+		System.out.println(response.getStatusLine());
+		System.out.println(response.getHeader("content-type"));
+		
+		//Pass and fail Scenario
+		int statusCode = response.getStatusCode();
+		Assert.assertEquals(statusCode, 202);
+		
 	}
 
 }
